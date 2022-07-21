1fdb1ba0fb221c999a1ff43d03e964fb3761a323
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-07-21 08:24:33 -0300

    Fix formulario con Vue

--- formulario-1/src/views/HomeView.vue
+++ formulario-1/src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form >
+    <input 
+      v-model="tarea.nombre"
+      type="text"
+      class="form-control"
+      placeholder="Ingrese un nombre"
+    />
+
+    <!-- checkbox -->
+    <div class="my-2">
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          class="form-check-input"
+          id="check-1"
+          v-model="tarea.categoria"
+          value="Javascript"
+        >
+        <label 
+          for="check-1"
+          class="form-check-label"
+        >Javascript</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          class="form-check-input"
+          id="check-2"
+          v-model="tarea.categoria"
+          value="Desarrollo"
+        >
+        <label 
+          for="check-2"
+          class="form-check-label"
+        >Desarrollo web</label>
+      </div>
+    </div>
+
+  </form>
+
+  <p>
+    {{ tarea }}
+  </p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
-  }
+   
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: ''
+      }
+    }
+  },
 }
 </script>
