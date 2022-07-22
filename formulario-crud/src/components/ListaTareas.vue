47e6f2ed5126cf91a695ac4998effe0e3d3231f0
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-07-22 10:32:16 -0300

    Formularios Crud

---
+++ formulario-crud/src/components/ListaTareas.vue
@@ -0,0 +1,39 @@
+<template>
+    
+  <table class="table table-hover text-center">
+    <thead>
+        <tr>
+            <th>#Id</th>
+            <th>Nombre</th>
+            <th>Categorias</th>
+            <th>Estado</th>
+            <th>Nro</th>
+            <th>Accion</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="tarea in tareas" :key="tarea.id">
+            <td>{{ tarea.id }}</td>
+            <td>{{ tarea.nombre }}</td>
+            <td>
+                <ul>
+                    <li v-for ="(categoria, index) in tarea.categorias" :key="index">{{ categoria }}</li>
+                </ul> 
+            </td>
+            <td>{{ tarea.estado }}</td>
+            <td>{{ tarea.numero }}</td>
+            <td>acccion</td>
+        </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed:{
+        ...mapState(['tareas'])
+    }
+}
+</script>
--- formulario-crud/src/views/HomeView.vue
+++ formulario-crud/src/views/HomeView.vue
@@ -1,94 +1,33 @@
 <template>
   <form @submit.prevent="procesarFormulario">
-    <!-- Input text -->
-    <div class="mt-4">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Ingrese un nombre"
-        v-model.trim="tarea.nombre"
-      />
-    </div>
-
-    <!-- checkbox -->
-    <div class="mt-4">
-      <div class="form-check form-check-inline">
-        <input
-          type="checkbox"
-          class="form-check-input"
-          id="check-1"
-          value="Javascript"
-          v-model="tarea.categorias"
-        />
-        <label for="check-1" class="form-check-label"> Javascript</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input
-          type="checkbox"
-          class="form-check-input"
-          id="check-2"
-          value="Desarrollo"
-          v-model="tarea.categorias"
-        />
-        <label for="check-2" class="form-check-label"> Desarrollo web</label>
-      </div>
-    </div>
-
-    <!-- radio -->
-    <div class="mt-4">
-      <div class="form-check form-check-inline">
-        <input
-          type="radio"
-          class="form-check-input"
-          id="inlineRadio1"
-          value="urgente"
-          v-model="tarea.estado"
-        />
-        <label class="form-check-label" for="inlineRadio1"> Urgente</label>
-      </div>
-
-      <div class="form-check form-check-inline">
-        <input
-          type="radio"
-          class="form-check-input"
-          id="inlineRadio2"
-          value="relax"
-          v-model="tarea.estado"
-        />
-        <label class="form-check-label" for="inlineRadio2"> Relax </label>
-      </div>
-    </div>
-
-    <!-- Number -->
-    <div class="mt-4">
-      <input
-        type="number"
-        class="form-control"
-        placeholder="numero"
-        v-model.number="tarea.numero"
-      />
-    </div>
-
-    <!-- Button Submit -->
-    <div class="mt-4">
-      <button type="submit" :disabled="bloquear" class="btn btn-info">
-        Procesar
-      </button>
-    </div>
+    <Input :tarea="tarea"/>
   </form>
 
   <div class="mt-4">
-    {{ tarea }}
+    <ListaTareas/>
   </div>
 </template>
 
 <script>
+
+import { nanoid } from 'nanoid'
+import {mapActions} from 'vuex'
+
+import Input from '@/components/Input';
+import ListaTareas from '@/components/ListaTareas';
+
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Input,
+    ListaTareas,
+    ListaTareas
+},
   data() {
     return {
       tarea: {
+        id: '',
         nombre: "",
         categorias: [],
         estado: "",
@@ -97,11 +36,20 @@ export default {
     };
   },
   methods: {
-    procesarFormulario() {
-      console.log(this.tarea);
+    ...mapActions(['setTareas']),
+
+    procesarFormulario() {      
+
+      // Generar Id
+      this.tarea.id = nanoid();
+
+      // Enviar tarea a Vuex
+      this.setTareas(this.tarea)
+
 
       // Una vez enviados los datos, los puedo limpiar
       this.tarea = {
+        id: '',
         nombre: "",
         categorias: [],
         estado: "",
@@ -110,16 +58,6 @@ export default {
       
     },
   },
-  // Uso esta funcion para validar
-  computed: {
-    bloquear() {
-      return this.tarea.nombre.trim() === "" ||
-        this.tarea.categorias.length === 0 ||
-        this.tarea.estado.trim() === "" ||
-        this.tarea.numero == 0
-        ? true
-        : false;
-    },
-  },
+  
 };
 </script>
