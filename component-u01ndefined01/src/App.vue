6cfcae78c1552871e83bdf7bc78ebe47086c83e6
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-09-29 11:50:12 -0300

    Componentes setup

---
+++ component-u01ndefined01/src/App.vue
@@ -0,0 +1,40 @@
+
+<script setup>
+import ButtonCounter from "./components/ButtonCounter.vue";
+import BlogPost from "./components/BlogPost.vue";
+</script>
+
+
+<template>
+  <div class="container">
+    <h3>App Contador</h3>
+
+    <ButtonCounter />
+    <button-counter></button-counter>
+
+    <BlogPost 
+      title="Post 1"
+      id = "Sub 1"
+      body = "Descripcion 1"
+      colorText="primary"
+      > 
+    </BlogPost>
+
+    <BlogPost 
+      title="Post 2"
+      id = "Sub 2"
+      body = "Descripcion 2"
+      colorText="info"
+      > 
+    </BlogPost>
+
+    <BlogPost 
+      title="Post 3"
+      id = "Sub 3"
+      body = "Descripcion 3"
+      colorText="success"
+      > 
+    </BlogPost>
+  </div>
+</template>
+
---
+++ component-u01ndefined01/src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+    const props = defineProps(
+        [
+            "title",
+            "id",
+            "body",
+            "colorText",
+        ]
+    )
+
+    // Ejemplo se puede escribir en consola, calcular, etc ... 
+
+    console.log(props.title);
+
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title" :class="`text-${colorText}`"> {{ id }} - {{ title }}</h5>
+      <p> {{ body }} </p>
+    </div>
+  </div>
+</template>
