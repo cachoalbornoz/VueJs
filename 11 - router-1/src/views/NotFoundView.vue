df8716a947ff73604ed15fbabf1c00386046c932
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-10-05 13:30:41 -0300

    Router component finalizado

---
+++ 11 - router-1/src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+import { useRouter } from "vue-router"    
+const router = useRouter()
+
+const back = () => {
+    router.push("/pokemons")
+}
+
+</script>
+
+<template>
+    <h1>404</h1>
+
+    <button class="btn btn-outline-primary" @click="back">
+        Regresar
+    </button>
+
+</template>
--- 11 - router-1/src/views/PokeView.vue
+++ 11 - router-1/src/views/PokeView.vue
@@ -2,7 +2,7 @@
 
 import { ref } from "vue";
 import axios from "axios";
-import {useRoute, useRouter} from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 
 const route = useRoute()
 const router = useRouter()
@@ -11,23 +11,21 @@ const poke = ref({})
 
 
 const back = () => {
-
     router.push("/pokemons")
 }
 
 
-const getData = async ()=>{
+const getData = async () => {
 
     try {
-        const {data} = await axios.get(
+        const { data } = await axios.get(
             `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
         )
-        console.log(data.sprites);
-        poke.value = data.sprites        
+        poke.value = data
     } catch (error) {
         console.log(error);
+        poke.value = null;
     }
-
 }
 
 
@@ -35,12 +33,19 @@ getData()
 </script>
 
 <template>
-    
-    <img :src="poke.front_default" alt="" />
-    
-    <h2>Poke name : {{ $route.params.name }}</h2>  
 
-    <button @click="back" > Volver </button>
+    <div v-if="poke" class="row">
+        <div class="col-12">
+            <img :src="poke.sprites?.front_default" alt="" />
+            <h2>Poke name : {{ $route.params.name }}</h2>
+        </div>
+    </div>
 
+    <div v-else class="row">        
+        <div class="col-12">
+            <h2> No existe el pokemon </h2>
+        </div>
+    </div>
+    <button @click="back" class="btn btn-outline-primary"> Volver </button>
 
 </template>
