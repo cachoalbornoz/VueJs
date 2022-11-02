ed59d8e53e9d5ee373cff0f2181bcd9c3f333ca0
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-11-02 19:40:45 -0300

    Eliminar documentos de FireStore

--- 14 - firebase -app-1/src/store/database.js
+++ 14 - firebase -app-1/src/store/database.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore/lite"
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, query, where } from "firebase/firestore/lite"
 import { defineStore } from "pinia";
 import { nanoid } from "nanoid";
 
@@ -53,13 +53,47 @@ export const useDatabaseStore = defineStore('database', {
                     user: auth.currentUser.uid
                 }
                 const docRef = await addDoc(collection(db, "urls"), objetoDoc)
-                console.log(docRef);
+
+                this.documents.push({
+                    ...objetoDoc,
+                    id: docRef.id, 
+                })
 
             } catch (error) {
                 console.log(error);
             }finally{
                 this.loading = false
             }
+        },
+
+        async eliminar(id){
+
+            this.loadingDoc = true
+            try {
+                
+                const docRef = doc(db, 'urls', id) 
+
+                // Verifico si existe el documento
+                const docUrl = await getDoc(docRef)
+                if(!docUrl.exits()){
+                    throw new Error("No existe el documento")
+                }
+
+                // Verifico si el usuario logueado coincide con el dueño del documento
+                if(docUrl.data().user !== auth.currentUser.uid){
+                    throw new Error("Usuario no autorizado")
+                }
+
+                // Borro el documento
+                await deleteDoc(docRef)
+                this.documents = this.documents.filter(item => item.id !== id)
+
+            } catch (error) {
+                console.log(error.message);
+            }finally{
+                this.loadingDoc = false
+            }
+            
         }
 
     }
--- 14 - firebase -app-1/src/views/Documents.vue
+++ 14 - firebase -app-1/src/views/Documents.vue
@@ -1,12 +1,14 @@
 <template>
 
-    <p v-if="databaseStore.loadingDoc">loading</p>
+    <p v-if="databaseStore.loadingDoc">Processing ... </p>
 
     <ul v-else>
         <li v-for="item of databaseStore.documents" :key="item.id">
             {{ item.id }} - {{ item.name }}
             <br>
             {{ item.short }}
+            <br>
+            <button @click="databaseStore.eliminar(item.id)">Eliminar</button>
         </li>
     </ul>
     
@@ -15,6 +17,7 @@
 <script setup>
 
 import { useDatabaseStore } from '../store/database';
+
 const databaseStore = useDatabaseStore();
 databaseStore.getUrls()
 
