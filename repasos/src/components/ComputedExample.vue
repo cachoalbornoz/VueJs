241604943feac3bc75295ac8136fa13acb229c81
Guillermo Albornoz <cachoalbornoz@gmail.com>
2022-12-19 11:27:25 -0300

    Fix Directivas

--- repasos/src/components/ComputedExample.vue
+++ repasos/src/components/ComputedExample.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h3>Calculo de Iva</h3>
+        <h3 v-custom-font.lg.red>Calculo de Iva</h3>
         <input type="number" v-model="subtotal" />
 
-        <h3>Total Iva : {{ totalIva }} ({{ iva }} %)</h3>
+        <h3 v-font-size>Total Iva : {{ totalIva }} ({{ iva }} %)</h3>
 
-        <h3>Total a pagar: {{ total }}</h3>
+        <h3 v-custom-size:xxl>Total a pagar: {{ total }}</h3>
     </div>
 
 </template>
@@ -17,7 +17,7 @@ import { ref, computed } from 'vue'
 const iva = 21
 let subtotal = ref(0)
 const totalIva = computed(() => (subtotal.value * iva) / 100)
-const total = computed(() => subtotal.value + totalIva.value )
+const total = computed(() => subtotal.value + totalIva.value)
 
 </script>
 
--- repasos/src/main.js
+++ repasos/src/main.js
@@ -1,4 +1,63 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.directive('font-size', {
+    beforeMount(el, binding) {
+        el.style.fontSize = "40px"
+    },
+})
+
+app.directive('custom-size', {
+    beforeMount(el, binding) {
+
+        let size = 18
+
+        switch (binding.arg) {
+            case "sm":
+                size = 10
+                break;
+            case "md":
+                size = 14
+                break;
+            case "xl":
+                size = 24
+                break;
+            case "xxl":
+                size = 48
+                break;
+        }
+
+        el.style.fontSize = size + "px"
+    },
+})
+
+app.directive('custom-font', {
+    beforeMount(el, binding) {
+
+        let size = 18
+
+        if (binding.modifiers.sm) {
+            size = 10
+        } else if (binding.modifiers.md) {
+            size = 18
+        } else if (binding.modifiers.lg) {
+            size = 44
+        }else if (binding.modifiers.xxl) {
+            size = 64
+        }
+
+        el.style.fontSize = size + "px"
+
+        if (binding.modifiers.red) {
+            el.style.color =  '#ff0000'
+        } else if (binding.modifiers.green) {
+            el.style.color =  '#00ff00'
+        } else if (binding.modifiers.blue) {
+            el.style.color =  '#0000ff'
+        }
+    },
+})
+
+app.mount('#app')
